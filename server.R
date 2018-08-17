7c145c2bae5501006d4e514f9c614b37b75a535b
Thomas J. Leeper <thosjleeper@gmail.com>
2018-08-17 08:42:05 -0500

    add options and per-page plot to app

--- server.R
+++ server.R
@@ -4,6 +4,7 @@
 
 library("shiny")
 library("pdftools")
+library("ggplot2")
 
 #' @title Word Count a PDF
 #' @description Obtain a Word Count from a PDF
@@ -140,13 +141,42 @@ function(
 }
 
 server <- function(input, output) {
-    
-    output$grand_total <- renderText({
+    count <- reactive({
         infile <- input$infile
         if (is.null(infile)) {
-            return("")
+            return(NULL)
         }
-        paste("Word Count:", sum(word_count(infile$datapath, verbose = FALSE)$words, na.rm = TRUE))
+        word_count(infile$datapath,
+                   pages = if (is.null(input$pages)) NULL else eval(parse(text = paste0("c(", input$pages, ")"))),
+                   count_numbers = input$count_numbers,
+                   count_captions = input$count_captions,
+                   count_equations = input$count_equations,
+                   split_hyphenated = input$split_hyphenated,
+                   split_urls = input$split_urls,
+                   verbose = FALSE)
     })
     
+    # total word count
+    output$grand_total <- renderText({
+        counts <- count()
+        if (is.null(counts)) {
+            return("")
+        }
+        paste("Word Count:", sum(counts$words, na.rm = TRUE))
+    })
+    
+    # graph of word counts by page
+    output$barplot <- renderPlot({
+        counts <- count()
+        if (is.null(counts)) {
+            return(NULL)
+        }
+        ggplot(counts, aes(x = page, y = words)) +
+          xlab("Page") +
+          ylab("Words per page") +
+          scale_x_reverse() + 
+          geom_col() +
+          coord_flip() +
+          theme_minimal()
+    })
 }
--- ui.R
+++ ui.R
@@ -5,12 +5,23 @@
 library("shiny")
 
 ui <- fluidPage(
-  mainPanel(
-    headerPanel("Count Words in a PDF Document"),
-    p(""),
-    p("Upload your file here:"),
-    fileInput("infile", label = NULL, accept = "application/pdf"),
-    p(""),
-    textOutput("grand_total")
+  titlePanel("Count Words in a PDF Document"),
+  sidebarLayout(
+    sidebarPanel(
+      strong(p("Upload your file here:")),
+      fileInput("infile", label = NULL, accept = "application/pdf"),
+      textInput("pages", "Page numbers to count:", "", width = "70%"),
+      strong(p("Additional options:")),
+      checkboxInput("count_numbers", "Count numbers?", TRUE),
+      checkboxInput("count_captions", "Count table/figure captions?", FALSE),
+      checkboxInput("count_equations", "Count equation lines?", FALSE),
+      checkboxInput("split_hyphenated", "Split hyphenated words?", FALSE),
+      checkboxInput("split_urls", "Tokenize URLs?", FALSE)
+    ),
+    mainPanel(
+      strong(textOutput("grand_total")),
+      p(""),
+      plotOutput("barplot", width = "100%", height = "600px")
+    )
   )
 )
